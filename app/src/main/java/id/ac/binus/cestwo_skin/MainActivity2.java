cfb617f520b9bcbc7c1b652f2ab3a3b9e5d9820a
william <jeremiah.wsebastian@gmail.com>
2023-12-10 22:51:16 +0700

    establishing landing page

---
+++ app/src/main/java/id/ac/binus/cestwo_skin/MainActivity2.java
@@ -0,0 +1,16 @@
+package id.ac.binus.cestwo_skin;
+
+import android.os.Bundle;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
